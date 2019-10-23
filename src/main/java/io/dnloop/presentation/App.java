33e8b56fda7943cd2014031c187987d08aa54a26
dnloop <29187104+dnloop@users.noreply.github.com>
2019-10-23 17:41:46 -0300

    integrated spring boot with afterburner.fx

--- src/main/java/io/dnloop/presentation/App.java
+++ src/main/java/io/dnloop/presentation/App.java
@@ -1,14 +1,40 @@
-package io.dnloop.printcalc;
+package io.dnloop.presentation;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import com.airhacks.afterburner.injection.Injector;
+
+import io.dnloop.presentation.printerCalculator.PrinterCalculatorView;
+import io.dnloop.validator.EntityValidator;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 @SpringBootApplication
-public class PrintCalcApplication {
+public class App extends Application {
 
-    public static void main(String[] args) {
-	Application.launch(App.class, args);
+    private ConfigurableApplicationContext springContext;
+
+    @Override
+    public void init() throws Exception {
+	EntityValidator.buildValid();
+	springContext = SpringApplication.run(App.class);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+	PrinterCalculatorView mainView = new PrinterCalculatorView();
+	stage.setScene(new Scene(mainView.getView()));
+	stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+	EntityValidator.closeValid();
+	Injector.forgetAll();
+	springContext.close();
     }
 
 }
---
+++ src/main/java/io/dnloop/presentation/Main.java
@@ -0,0 +1,14 @@
+package io.dnloop.presentation;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javafx.application.Application;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+	Application.launch(App.class, args);
+    }
+
+}
